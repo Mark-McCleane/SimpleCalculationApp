0e25c408064418d9829e06d94d63358acbb0e3c7
mcclmar <mccleanemark@gmail.com>
2024-06-30 19:26:58 +0100

    moved MainScreen to separate screen and added a scaffold toolbar

--- app/src/main/java/com/example/aerlingusassessment/MainActivity.kt
+++ app/src/main/java/com/example/aerlingusassessment/MainActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.aerlingusassessment.ui.MainScreen
 import com.example.aerlingusassessment.ui.theme.AerLingusAssessmentTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,15 +33,6 @@ class MainActivity : ComponentActivity() {
                     MainScreen(innerPadding)
                 }
             }
-        }
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun MainScreenPreview() {
-        AerLingusAssessmentTheme {
-            val padding = PaddingValues(10.dp)
-            MainScreen(padding)
         }
     }
 }
